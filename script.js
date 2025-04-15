e33fddf75bb36b842e4eeb21b19d4f80aed73651
claraharguindey <clarisaharguindey@gmail.com>
2025-04-15 16:25:03 +0200

    add wip

---
+++ script.js
@@ -0,0 +1,92 @@
+// ml5.js: Object Detection with COCO-SSD (Webcam)
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/learning/ml5/1.3-object-detection.html
+// https://youtu.be/QEzRxnuaZCk
+
+// p5.js Web Editor - Image: https://editor.p5js.org/codingtrain/sketches/ZNQQx2n5o
+// p5.js Web Editor - Webcam: https://editor.p5js.org/codingtrain/sketches/VIYRpcME3
+// p5.js Web Editor - Webcam Persistence: https://editor.p5js.org/codingtrain/sketches/Vt9xeTxWJ
+
+// let img;
+let video;
+let detector;
+let detections = [];
+let translations = {
+  "person": "persona",
+  "bottle": "botella",
+  "book": "libro",
+  "clock": "reloj",
+  "cup": "taza",
+  "keyboard": "teclado",
+  "laptop": "portátil",
+  "painting": "cuadro",
+  "chair": "silla",
+  "couch": "sofá",
+  "plant": "planta",  
+  "tree": "árbol",    
+  "kite": "cometa", 
+  "animal": "animal",  
+  "article": "artículo", 
+  "bank": "banco", 
+  "box": "caja", 
+  "computer": "computadora", 
+  "flower": "flor", 
+  "cell phone": "teléfono móvil",
+  "fan": "ventilador"
+};
+
+document.getElementById("translationForm").addEventListener("submit", function(e) {
+  e.preventDefault();  // Evita que el formulario recargue la página
+  let selectedLabel = document.getElementById("objectSelect").value;
+  let newTranslation = document.getElementById("newTranslation").value.trim();
+
+  // Actualizar el diccionario de traducciones si el campo no está vacío
+  if (newTranslation) {
+    translations[selectedLabel] = newTranslation;
+  }
+});
+
+
+function preload() {
+  // img = loadImage('dog_cat.jpg');
+  detector = ml5.objectDetector('cocossd');
+}
+
+function gotDetections(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  detections = results;
+  detector.detect(video, gotDetections);
+}
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+  detector.detect(video, gotDetections);
+}
+
+
+function draw() {
+  image(video, 0, 0);
+
+  for (let i = 0; i < detections.length; i++) {
+    let object = detections[i];
+    let label = object.label;
+    
+    if (translations[label]) {
+      label = translations[label];
+    }
+    
+    stroke(0, 255, 0);
+    strokeWeight(4);
+    noFill();
+    rect(object.x, object.y, object.width, object.height);
+    noStroke();
+    fill(255);
+    textSize(24);
+    text(label, object.x + 10, object.y + 24);
+  }
+}
